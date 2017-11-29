52d4c327ce4e833299456e28b676865813780c62
sergio.roldan <sergioroldan.macia@gmail.com>
2017-11-29 21:02:11 +0100

    QuotaProxie Fix 9

--- server.js
+++ server.js
@@ -30,10 +30,19 @@ app.post('/linkedIn', function(req, res){
 
 app.get('/researchGate', function(req, res){
 
+  var options = {
+  url: 'https://www.researchgate.net/profile/Ramon_Bragos',
+  headers: {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:57.0) Gecko/20100101 Firefox/57.0'
+  }
+};
 
-  fixieRequest('https://www.researchgate.net/profile/Ramon_Bragos', (err, res, body) => {
-    console.log(res.statusCode);
-  });
+function callback(error, response, body) {
+  console.log(response.statusCode);
+}
+
+fixieRequest(options, callback);
+
 
   /*var str = '';
 
