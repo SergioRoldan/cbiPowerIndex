376ef95c4562aafab43820aaf4d3b80a9517f0b6
sergio.roldan <sergioroldan.macia@gmail.com>
2017-11-29 18:55:29 +0100

    I'm idiot

--- public/js/app.js
+++ public/js/app.js
@@ -4,7 +4,7 @@ $("#submitButton").on('click', function(event) {
   var userName = $("#inputEmail").val();
   var userPassword = $("#inputPassword").val();
 
-  $.post("http://localhost:8080/linkedIn", {
+  $.post("https://cbi-test.herokuapp.com/linkedIn", {
     user: userName,
     password: userPassword
   } ,function(data, status) {
@@ -18,7 +18,7 @@ $("#rgButton").on('click', function(event) {
 
   event.preventDefault();
 
-  $.get("http://localhost:8080/researchGate", function(data, status) {
+  $.get("https://cbi-test.herokuapp.com/researchGate", function(data, status) {
     console.log("Status: " + status);
     console.log("Data: " + data);
     dataConverted = data.replace(/<(?:.|\n)*?>/gm, '');
